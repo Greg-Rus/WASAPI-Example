07e71f567fab3ed3957d80f26a2368aa66145db3
Greg-Rus <grzegorz.rus@fluffyfairygames.com>
2018-03-15 19:27:38 +0100

    Reactive TimedUpdateLoop

--- Assets/CubeView.cs
+++ Assets/CubeView.cs
@@ -22,7 +22,40 @@ public class CubeView : MonoBehaviour
             meshRenderer.material.color = Color.Lerp(color, normalColor, t);
             if(t >= 1f) updateSubsctiption.Dispose();
         });
+        
     }
 
+    private TimedUpdateLoop _timedUpdateLoop;
 
+    public void ColorCube(Color32 color, float time = 3)
+    {
+        _timedUpdateLoop = new TimedUpdateLoop(time, () => meshRenderer.material.color = Color.Lerp(color, normalColor, t) );
+    }
+
+}
+
+public class TimedUpdateLoop : IDisposable
+{
+    private readonly Action<float> _excecuteEveryUpdateAction;
+    private readonly IDisposable _subscription;
+
+    private float _deadline;
+
+    public TimedUpdateLoop(float timeSpanInSeconds, Action<float> excecuteEveryUpdateAction)
+    {
+        _excecuteEveryUpdateAction = excecuteEveryUpdateAction;
+        _deadline = Time.time + timeSpanInSeconds;
+
+        var observable = Observable.EveryUpdate().TakeWhile(_ => Time.time > _deadline);
+        _subscription = observable.Subscribe(_ =>
+        {
+            _excecuteEveryUpdateAction();
+        });
+    }
+
+    public void Dispose()
+    {
+        if(_subscription != null)
+            _subscription.Dispose();
+    }
 }
